f0ae4a292d4f7104eda1872f3f165bf94ece3c41
Rodolfo Ferreira <rodolfogf96@gmail.com>
2026-06-26 16:55:08 -0300

    feat(base-dialog): new form sizes

--- src/components/base-dialog.tsx
+++ src/components/base-dialog.tsx
@@ -12,6 +12,11 @@ const sizeClasses = {
   lg: "sm:max-w-lg",
   xl: "sm:max-w-xl",
   "2xl": "sm:max-w-2xl",
+  "3xl": "sm:max-w-3xl",
+  "4xl": "sm:max-w-4xl",
+  "5xl": "sm:max-w-5xl",
+  "6xl": "sm:max-w-6xl",
+  "7xl": "sm:max-w-7xl",
 } as const
 
 export type BaseDialogSize = keyof typeof sizeClasses
